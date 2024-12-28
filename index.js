1adc3ae24cb1108cc3103cd774c934ec6cf68f1f
mehmet.tokmak <mehmet.tokmak@beyn.com.tr>
2024-12-28 19:30:46 +0300

    domain folder created

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
 const app = require("express")();
 const port = 80;
 
 const server = http.createServer(app);
 
-app.listen(port, () => {
-  console.log(`${port} is listening ...`);
+const createFolderIfNotExists = (domainFolder) => {
+  const domainPath = path.join(__dirname, domainFolder);
+  if (!fs.existsSync(domainPath)) {
+    const exists = fs.mkdirSync(domainFolder);
+    if (!exists) {
+      console.log(`domainPath created: ${domainFolder}`);
+      return false;
+    }
+    console.log(`domainPath already exists: ${domainFolder}`);
+  }
+};
+
+app.use("/", (req, res, next) => {
+  const { host } = req.headers;
+  console.log({ host });
+  createFolderIfNotExists(host);
+  next();
 });
 
 app.get("/", (req, res) => {
   res.send("Welcome");
+});
+
+server.listen(port, () => {
+  console.log(`${port} is listening ...`);
 });
